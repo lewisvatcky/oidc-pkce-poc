2848efbcdf949f6bff0a96be113e882c4256ede1
Lewis Vatcky <lewis.vatcky@gameonsports.com.au>
2021-04-23 15:55:47 +1000

    add popup signin

--- signin.js
+++ signin.js
@@ -1,2 +1,3 @@
 
 document.querySelector('#signin').addEventListener('click', () => window.UserManager.signinRedirect());
+document.querySelector('#signinpopup').addEventListener('click', () => window.UserManager.signinPopup());
