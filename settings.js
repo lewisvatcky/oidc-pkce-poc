a3b4a8fd1c3e2e66d968fe011e9c455ae103c19d
Lewis Vatcky <lewis.vatcky@gameonsports.com.au>
2021-04-23 14:30:18 +1000

    added extra query params

--- settings.js
+++ settings.js
@@ -1,12 +1,17 @@
 const settings = {
-  authority: 'https://dev-18xi1sx5.au.auth0.com',
-  client_id: 'whlnBxWXjJ30x8EnxEEK2gt1jk1j6hGe',
-  redirect_uri: 'http://localhost:8000/callback.html',
-  response_type: 'code',
-  scope: 'openid',
+  authority: 'https://dev-18xi1sx5.au.auth0.com', // The Cricket Australia Auth URL (Gigya)
+  client_id: 'whlnBxWXjJ30x8EnxEEK2gt1jk1j6hGe', // The Client ID for the app registered against Gigya
+  redirect_uri: 'http://localhost:8000/callback.html', // The URL we want to return to once authenticated via Gigya
+  response_type: 'code', // This ensures this request is PKCE
+  scope: 'openid', // openid is the only scope we will have access to
   filterProtocolClaims: true,
   loadUserInfo: false,
-  response_mode: 'query'
+  response_mode: 'query', // Used to make sure response_type: 'code' works as expected
+  extraQueryParams: { // Any extra parameters to send to the /authorize request. If we are prefilling information, this is where we can pass the information through.
+    firstname: "Lewis",
+    lastname: "Vatcky",
+    email: 'theusers@email.com'
+  }
 };
 
 window.Settings = settings;
